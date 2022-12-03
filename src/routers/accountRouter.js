f4c056231759cdf9e2c5849e6b84e323242ddbc4
Edson <edsonmac@Edson-desktop.localdomain>
2022-12-03 20:24:27 -0300

    Fixing middlware import

--- src/routers/accountRouter.js
+++ src/routers/accountRouter.js
@@ -2,12 +2,12 @@ const router = require('express').Router();
 const accountController = require('../controllers/accountController');
 const validateAccount = require('../middlewares/validateAccount');
 const validateToken = require('../middlewares/validateToken');
-const validadeDeposit = require('../middlewares/validadeDeposit');
+const validateDeposit = require('../middlewares/validateDeposit');
       
 const { createAccount, getSaldo, createDeposito } = accountController;
 
 router.post('/', validateAccount, createAccount);
 router.get('/saldo', validateToken, getSaldo);
-router.post('/deposito', validateToken, validadeDeposit, createDeposito);
+router.post('/deposito', validateToken, validateDeposit, createDeposito);
 
 module.exports = router;
