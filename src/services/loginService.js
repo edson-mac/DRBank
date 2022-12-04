011713f381bf1ca9e653a15adad0d897b8196e90
Edson <edsonmac@Edson-desktop.localdomain>
2022-12-04 16:19:21 -0300

    fixing login response

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -4,7 +4,7 @@ const tokenGenerator = require('../jwt');
 const newLogin = async (cpf, name) => {
     const findUser = await accountModel.findAccount(cpf)
     if (!findUser || name.toLowerCase() !== findUser.name.toLowerCase()) {
-      return { code: 400, response: { message: 'CPF ou Nome incorretos' } }; 
+      return { code: 400, response: 'CPF ou Nome incorretos' }; 
     }
     const token = tokenGenerator({ cpf, name });
     return { code: 200, response: { token, name } };
