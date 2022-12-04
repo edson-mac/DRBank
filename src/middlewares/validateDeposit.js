da5411edc1f05dc1f22f99b3c251143e7e3b02a0
Edson <edsonmac@Edson-desktop.localdomain>
2022-12-04 18:55:47 -0300

    fixing login response3

--- src/middlewares/validateDeposit.js
+++ src/middlewares/validateDeposit.js
@@ -3,9 +3,7 @@ module.exports = async (req, res, next) => {
 
     if (!deposito) return res.status(400).json({ message: '"deposito" is required' });
 
-    if (!transferValue) return res.status(400).json({ message: '"transferValue" is required' });
-
-    if (!/^\d+$/.test(transferValue)) return res.status(400).json({ message: 'O deposito deve conter apenas números' });
+    if (!/^\d+$/.test(deposito)) return res.status(400).json({ message: 'O deposito deve conter apenas números' });
 
     next();
 };
